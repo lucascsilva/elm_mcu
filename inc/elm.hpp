8339d327aa650394e797737afac8275fbb217824
Lucas Silva <lucas.silva@ymail.com>
2017-10-08 20:26:24 -0300

    Modify Elm class
    Private members changed to pointers;
    Constructor allocates memory space to the private memebers;
    Code for method SetRandomWeights.

--- inc/elm.hpp
+++ inc/elm.hpp
@@ -3,24 +3,30 @@
 
 #include "matrix_manipulation.h"
 
-#define NUM_INPUT_NEURONS
-#define NUM_HIDDEN_NEURONS
-#define NUM_OUTPUT_NEURONS
+#define NUM_INPUT_NEURONS   3
+#define NUM_HIDDEN_NEURONS  10
+#define NUM_HIDDEN_LAYERS   1
+#define NUM_OUTPUT_NEURONS  4
+#define NUM_SAMPLES         20
 
 
 class Elm 
 {
     private:
 
-    gsl_matrix random_weights;
-    gsl_matrix output_weights;
+    gsl_matrix* random_weights;
+    gsl_matrix* output_weights;
+    gsl_matrix* hidden_layer_outputs;
 
     public:
-
+    
+    Elm();
     float ActivationFunction(float arg);
     void TrainElm(const gsl_matrix &batch_input, const gsl_matrix &target);
     void NetworkOutput(const gsl_matrix &input, gsl_matrix* output);
-
+    void SetRandomWeights(const float *weights);
+    void HiddenLayerOutput(const gsl_matrix &samples);
+    
 };
 
 #endif
--- src/elm.cpp
+++ src/elm.cpp
@@ -1,2 +1,23 @@
 #include "elm.hpp"
 
+
+Elm::Elm()
+{
+    random_weights = gsl_matrix_alloc(NUM_HIDDEN_LAYERS, NUM_HIDDEN_NEURONS);
+    output_weights = gsl_matrix_alloc(NUM_HIDDEN_NEURONS, NUM_OUTPUT_NEURONS);
+    hidden_layer_outputs = gsl_matrix_alloc(NUM_SAMPLES, NUM_HIDDEN_NEURONS);
+}
+
+void Elm::SetRandomWeights(const float *weights)
+{
+    size_t weight_counter=0;
+    for(size_t row_counter=0; row_counter < random_weights->size1; row_counter++)
+    {
+        for(size_t col_counter=0; col_counter<random_weights->size2; col_counter++)
+        {
+            gsl_matrix_set(random_weights,row_counter, col_counter, weights[weight_counter++]);
+        }   
+    }
+}
+
+
